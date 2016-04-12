9a722ca510e922ad5ce34f82d8f17a330a37ffa4
iamhades <Slimeria37@gmail.com>
2016-04-12 17:07:56 +0800

    项目编译配置文件提交

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    root: true,
+    extends: 'standard',
+    plugins: [
+        'html'
+    ],
+    globals:{
+        'Vue':true
+    },
+    rules: {
+        'arrow-parens': 0,
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'space-before-function-paren': [0, 'never'],
+        'indent':0,
+        'eol-last':0,
+        'spaced-comment':[2, "always"],
+        'comma-spacing':[2, {"before": false, "after": true}],
+        'no-trailing-spaces':[2, { "skipBlankLines": true }],
+        'space-infix-ops':0,
+        'key-spacing':[0, { "beforeColon": false, "afterColon": true }],
+        'quotes':[1, "single"],
+        'padded-blocks':0,
+        'space-before-blocks':[2,'always'],
+        'eqeqeq': 2,
+        'no-trailing-spaces':1,
+        'no-multiple-empty-lines':2
+    }
+}
