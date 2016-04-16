190e80fcaacdeaddc3870e4777709fcb56123497
Slimeria <Slimeria37@gmail.com>
2016-04-17 03:58:54 +0800

    菜单适配，动效添加

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -9,8 +9,8 @@
           </button>
           <a class="navbar-brand hidden-sm" href="">VueTheme-Red</a>
         </div>
-        <div class="navbar-collapse" style="height: 1px;" :class="{collapse:isShow}">
-          <ul class="nav navbar-nav navbar-right">
+        <div v-el:menu class="navbar-collapse" style="height: 1px;" :class="{showbyopacity:isShow}">
+          <ul class="nav navbar-nav navbar-right" :class="{showtranslate:isShow}">
             <li v-for="menu in menus"><a href="#">{{menu.text}}</a></li>
             <li class="hidden-sm"><a href="#">About</a></li>
           </ul>
@@ -32,7 +32,7 @@
    },
    data() {
      return {
-        isShow:true
+        isShow:false
      }
    },
    created() {
@@ -41,6 +41,7 @@
    methods: {
      showMenu(){
         this.isShow=!this.isShow
+        // this.$els.menu.style
      }
    }
  }
@@ -48,11 +49,76 @@
 </script>
 <style type="text/css">
 
+
+.navbar-fixed-top .navbar-toggle{
+  z-index: 1;
+}
+.navbar-brand{
+  font-size: 25px;
+  padding-top: 21px;
+  color: black;
+  font-weight: bold;
+}
+
+.navbar-right{
+  background-color: white;
+}
+
 .navborder-red {
   border-bottom: 1px red solid;
 }
 .navborder-red .icon-bar {
   background-color: #888;
+}
+
+.navbar-nav>li>a{
+    font-family: "Raleway", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-weight: bold;
+    font-size: 11px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    color: #777777;
+    -webkit-transition: all .5s ease;
+    -moz-transition: all .5s ease;
+    transition: all .5s ease;
+    padding-bottom: 40px;
+    padding-top: 30px;
+    display: inline-block;
+    line-height: 14px;
+    cursor: pointer;
+    width: 100%;
+}
+
+.navbaranimal{
+    transform:translateY(100px);
+    transition: -webkit-transform 400ms cubic-bezier(0.215, 0.61, 0.355, 1) 0ms;
+    background-color: rgb(255, 255, 255);
+  }
+
+.showbyopacity{
+  opacity:1!important;
+}
+
+.showtranslate{
+  transform: translate3d(0px, 0px, 0px)!important;
+}
+
+@media (max-width: 780px) {
+  .navbar-nav>li>a{
+    padding-bottom: 15px;
+    padding-top: 15px;
+  }
+
+  .navbar-collapse {
+    opacity:0;
+    transition: opacity 800ms cubic-bezier(0.39, 0.575, 0.565, 1) 300ms;
+  }  
+
+  .navbar-collapse .navbar-right{
+    transform: translate3d(0px, -40px, 0px);
+    transition: -webkit-transform 500ms cubic-bezier(0, 1, 1, 0.73) 300ms;
+  }
+  
 }
 
 </style>
