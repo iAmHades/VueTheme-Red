7adac375da338057beb36ce450163c138f0bde42
Hades <Slimeria37@gmail.com>
2016-05-15 13:21:06 +0800

    添加组件编辑器文件

---
+++ editor/util/editor.js
@@ -0,0 +1,20 @@
+/**
+ *  组件的编辑器对象
+ *  type 组件类型
+ **/
+
+function Editor(type) {
+
+}
+
+Editor.prototype.editGrid = function(dom) {
+
+}
+
+Editor.prototype.editForm = function(dom) {
+
+}
+
+Editor.prototype.editMenu = function(dom) {
+
+}
