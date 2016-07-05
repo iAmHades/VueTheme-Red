dad103a259ba85abb99eec9498a55cc7e75ef329
Slimeria <Slimeria37@gmail.com>
2016-07-05 10:35:26 +0800

    修复input组件的样式问题

--- src/components/form/input.vue
+++ src/components/form/input.vue
@@ -109,7 +109,6 @@
   .input-lable {
     color: #9e9e9e;
     font-size: 14px;
-    margin-left: -80px;
     transition: .2s;
   }
 
@@ -117,6 +116,5 @@
     color: red;
     transform: translateY(-140%);
     font-size: 10px;
-    margin-left: -80px;
   }
 </style>
